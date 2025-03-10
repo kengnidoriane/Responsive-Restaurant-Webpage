8c6ad9804f6fd8e45685f9f4cf983793a3498545
kengnidoriane <dorianekengni@gmail.com>
2025-03-10 17:33:27 +0100

    add pictures in the site

--- js/main.js
+++ js/main.js
@@ -112,8 +112,8 @@
    */
   on('click', '.mobile-nav-toggle', function(e) {
     select('#navbar').classList.toggle('navbar-mobile')
+    this.classList.toggle('bi-x');
     this.classList.toggle('bi-list')
-    this.classList.toggle('bi-x')
   })
 
   /**
